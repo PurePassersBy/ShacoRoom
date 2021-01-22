417f4c88575745a2b5232a0b99d94042da2c995f
GanbatteKnight <614446871@qq.com>
2021-01-22 17:50:38 +0800

    修复了发送邮件的一个小bug 以及初步的框架设定（待填充）

--- authentication/loginGUi.py
+++
@@ -1,9 +0,0 @@
-import sys
-from time import sleep, strftime, localtime
-import threading
-
-from PyQt5.QtGui import QPixmap, QIcon, QTextCursor
-from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import QLabel, QMessageBox, QWidget
-
-
--- authentication/mailVerify.py
+++ authentication/mailVerify.py
@@ -13,18 +13,19 @@ class Mail():
         self.mailSender = sender
         self.mailPassword = password
         self.mailReceiver = receiver
+        self.verifCode
     def send(self):
-        verifyCode = self.generateCode()
+        self.verifyCode = self.generateCode()
         flag = True
         try:
-            msg = MIMEText('欢迎注册ShacoRoom 您的验证码为：' + verifyCode + '\n请在十五分钟内完成验证', 'plain', 'utf-8')      #邮件内容
+            msg = MIMEText('欢迎注册ShacoRoom 您的验证码为：' + str(verifyCode) + '\n请在十五分钟内完成验证', 'plain', 'utf-8')      #邮件内容
             msg['From'] = formataddr(["ShacoRoom", self.mailSender])            # 括号里的对应发件人邮箱昵称、发件人邮箱账号
             msg['To'] = formataddr(["Dear ShacoRoom User", self.mailReceiver])  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
             msg['Subject'] = "ShacoRoom注册验证码"                               # 邮件的主题，也可以说是标题
             server = smtplib.SMTP_SSL("smtp.qq.com", 465)                       # 发件人邮箱中的SMTP服务器，qqmail端口是465
             server.login(self.mailSender, self.mailPassword)                    # 括号中对应的是发件人邮箱账号、邮箱密码
             server.sendmail(self.mailSender, [self.mailReceiver,], msg.as_string())
-            server.quit()  # 关闭连接
+            server.quit()                                                       # 关闭连接
         except Exception:
             flag = False
         return flag
@@ -33,9 +34,14 @@ class Mail():
         code = random.randint(100000, 999999)                                   #生成六位随机数
         return code
 
+    def check(self):
+        pass
+
+
+
 if __name__ == '__main__':
     test = Mail('614446871@qq.com', 'rduygnlorlpgbeec', '614446871@qq.com')
     if test.send():
-        print("Tese success")
+        print("Test success")
     else:
         print("Something Wrong")
