141acb0d54d14282fe6abdb58c7a009d5be396a5
GanbatteKnight <614446871@qq.com>
2021-01-22 16:17:52 +0800

    发送验证邮件模块

---
+++ authentication/loginGUi.py
@@ -0,0 +1,9 @@
+import sys
+from time import sleep, strftime, localtime
+import threading
+
+from PyQt5.QtGui import QPixmap, QIcon, QTextCursor
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QLabel, QMessageBox, QWidget
+
+
