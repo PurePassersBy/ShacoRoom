30af7ae87c8d1c3917a2f3cba2b1f7ca7168cd5e
Craddy <1041396750@qq.com>
2021-01-25 14:23:08 +0800

    头像处理

---
+++ src/server/ResourceManager.py
@@ -0,0 +1,15 @@
+import socket
+import threading
+
+import pymysql
+
+class ResourceManager:
+
+    def __init__(self):
+        pass
+
+
+class PortraitManager(ResourceManager):
+
+    def __init__(self):
+        pass
