aca54b3ab233efe94a132c04a900d439534e723e
Craddy <1041396750@qq.com>
2021-01-27 15:04:51 +0800

    重构资源管理器（应以文件为单位管理）

--- src/server/ResourceManager.py
+++ src/server/ResourceManager.py
@@ -18,26 +18,5 @@ class ResourceManager:
         self.server.listen(50)
 
     def fetch_and_store(self, conn, path, file_name):
-        data = ''.encode()
-        while len(data) < PAYLOAD_SIZE:
-            data += self.server.recv(CHUNK)
-        packed_size = data[:PAYLOAD_SIZE]
-        data = data[PAYLOAD_SIZE:]
-        msg_size = struct.unpack("L", packed_size)[0]
-        while len(data) < packed_size:
-            data += self.server.recv(CHUNK)
-        nparr = np.array(bytearray(data[:msg_size]), np.uint8)
-        img_decode = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
+        pass
         # TODO: 有效存储
-
-
-
-class PortraitManager(threading.Thread, ResourceManager):
-
-    def __init__(self, addr):
-        super().__init__()
-
-    def run(self):
-        while True:
-            conn, addr = self.server.accept()
