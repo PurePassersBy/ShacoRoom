f2123e5fe94caa56eca2c7d4a70c79ed55097d1d
Craddy <1041396750@qq.com>
2021-01-27 15:30:46 +0800

    重构传参方式

--- src/server/main.py
+++ src/server/main.py
@@ -1,10 +1,18 @@
 from Manager import Manager
 from VideoManager import VideoManager
+from ResourceManager import ResourceManager
+
+SERVER_ADDRESS = ('0.0.0.0', 3976)
+VIDEO_SERVER_ADDRESS = ('0.0.0.0', 3977)
+AUDIO_SERVER_ADDRESS = ('0.0.0.0', 3978)
+RESOURCE_SERVER_ADDRESS = ('0.0.0.0', 3979)
+
 
 if __name__ == '__main__':
-    manager = Manager()
-    videoManager = VideoManager()
-    audioManager = VideoManager(('0.0.0.0', 3978))
+    manager = Manager(SERVER_ADDRESS)
+    videoManager = VideoManager(VIDEO_SERVER_ADDRESS)
+    audioManager = VideoManager(AUDIO_SERVER_ADDRESS)
+    resourceManager = ResourceManager(RESOURCE_SERVER_ADDRESS)
     manager.start()
     videoManager.start()
     audioManager.start()
