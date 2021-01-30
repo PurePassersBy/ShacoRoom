7f120b23e0b7a4b2b8c93c9bf464a9ec42c00531
GanbatteKnight <614446871@qq.com>
2021-01-30 23:07:51 +0800

    新DAO使用样例

--- authentication/test.py
+++ authentication/test.py
@@ -2,9 +2,9 @@ from authentication.DAO.dataBase import ConnectSQL
 import time
 
 SERVER_ADDRESS = ('39.106.169.58', 3980)
-
+TABLE_NAME = 'userinfo'
 if __name__ == '__main__':
-    table_name = 'userinfo'
+
     data_search = ['name', 'test']
     data_insert = ['test','213123123','123123@qad']
     data_delete = ['name', 'test']
@@ -12,13 +12,13 @@ if __name__ == '__main__':
     conn = ConnectSQL(SERVER_ADDRESS)
     # conn.search(table_name, data_search)
 
-    conn.insert(table_name, data_insert)
-    conn.search(table_name, data_search)
-    conn.delete(table_name, data_delete)
-    conn.search(table_name, data_search)
-    conn.insert(table_name, data_insert)
-    conn.edit(table_name, data_edit)
-    conn.search(table_name, data_search)
-    conn.delete(table_name, data_delete)
-    conn.search(table_name, data_search)
+    conn.insert(TABLE_NAME, data_insert)
+    conn.search(TABLE_NAME, data_search)
+    conn.delete(TABLE_NAME, data_delete)
+    conn.search(TABLE_NAME, data_search)
+    conn.insert(TABLE_NAME, data_insert)
+    conn.edit(TABLE_NAME, data_edit)
+    conn.search(TABLE_NAME, data_search)
+    conn.delete(TABLE_NAME, data_delete)
+    conn.search(TABLE_NAME, data_search)
 
