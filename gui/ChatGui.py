00d44f927cdb0bebcc0cb161dd58eda3d21d4caf
Craddy <1041396750@qq.com>
2021-02-04 15:42:17 +0800

    listwidget基本重构完成

--- gui/ChatGui.py
+++ gui/ChatGui.py
@@ -6,9 +6,9 @@ from time import sleep
 import threading
 import socket
 
-from PyQt5.QtGui import QPixmap, QIcon, QTextCursor
-from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import QLabel, QMessageBox, QWidget
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 
 from gui.VChat import Ui_Form
 from gui.SettingsGui import SettingsGui
@@ -18,7 +18,35 @@ SERVER_ADDRESS = ('39.106.169.58', 3976)
 VIDEO_SERVER_ADDRESS = ('39.106.169.58', 3977)
 AUDIO_SERVER_ADDRESS = ('39.106.169.58', 3978)
 RESOURCE_SERVER_ADDRESS = ('39.106.169.58', 3979)
+PORTRAIT_PATH = '../gui/resource/portrait/%s.jpg'
 TABLE_NAME = 'userinfo'
+
+class ReceiveMessageThread(QThread):
+    msg_pack = pyqtSignal(dict)
+    def __init__(self, chatter):
+        super().__init__()
+        self.chatter = chatter
+
+    def run(self):
+        while True:
+            try:
+                msg = self.chatter.recv(1024).decode()
+                msg_ls = msg.split(' ')
+                ltime = ' '.join(msg_ls[:2])
+                user_id = msg_ls[2]
+                msg = ' '.join(msg_ls[3:])
+                print(ltime)
+                msg_pack = {
+                    'time':ltime,
+                    'user_id':user_id,
+                    'message':msg
+                }
+                print(msg_pack)
+                self.msg_pack.emit(msg_pack)
+                sleep(0.1)
+            except Exception as e:
+                print('recv msg', e)
+                break
 
 class ChatGUI(QWidget,Ui_Form):
 
@@ -35,11 +63,11 @@ class ChatGUI(QWidget,Ui_Form):
 
         self.db_conn = db_conn
 
-        self.init_client_socket()
-        self.get_others_info()
+        self.init_client()
+        #self.get_others_info()
 
-        self.textEdit_msg_box.setReadOnly(True)
         self.textEdit.installEventFilter(self)
+        print('done')
 
     def _get_others_portrait(self):
         while True:
@@ -66,13 +94,13 @@ class ChatGUI(QWidget,Ui_Form):
         threading.Thread(target=(self._get_others_portrait)).start()
 
 
-    def init_client_socket(self):
+    def init_client(self):
         self.chatter = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.chatter.connect(SERVER_ADDRESS)
         self.chatter.send(str(self.id).encode())
-        receiver = threading.Thread(target=self.recv_message)
-        receiver.setDaemon(True)
-        receiver.start()
+        self.chatter_recv = ReceiveMessageThread(self.chatter)
+        self.chatter_recv.msg_pack.connect(self.show_message)
+        self.chatter_recv.start()
 
         self.portrait_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.portrait_client.connect(RESOURCE_SERVER_ADDRESS)
@@ -87,28 +115,33 @@ class ChatGUI(QWidget,Ui_Form):
         self.label_username.setText(self.userName)
         self.graphicsView.setStyleSheet(f"border-image: url({self.portrait});")
 
-    def recv_message(self):
+    def show_message(self, msg_pack):
         """
-        接收消息
+        展示信息
         :return:
         """
-        while True:
-            try:
-                msg = self.chatter.recv(1024).decode()
-                msg_ls = msg.split(' ')
-                ltime = ' '.join(msg_ls[:2])
-                user_id = msg_ls[2]
-                user_name = self.db_conn.search('userinfo', ['id', user_id])[0][1]
-                msg = ' '.join(msg_ls[3:])
-                print(ltime)
-                self.textEdit_msg_box.append(ltime)
-                self.textEdit_msg_box.append(
-                    f'<img src="../gui/resource/portrait/{user_id}.jpg" id="portrait" width=50 height=50/>{user_name}: ' + msg)
-                self.textEdit_msg_box.append('')
-                self.textEdit_msg_box.moveCursor(self.textEdit_msg_box.textCursor().End)
-            except Exception as e:
-                print(e)
-                break
+        print('in')
+        time_ = msg_pack['time']
+        user_id = msg_pack['user_id']
+        user_name = self.db_conn.search('userinfo', ['id', user_id])[0][1]
+        msg = msg_pack['message']
+        widget = QWidget()
+        layout_main = QHBoxLayout()
+        layout_msg = QVBoxLayout()
+        portrait = QLabel()
+        portrait.setFixedSize(50,50)
+        img = QPixmap(PORTRAIT_PATH%user_id).scaled(50,50)
+        portrait.setPixmap(img)
+        layout_msg.addWidget(QLabel(f'{time_}  {user_name}'))
+        layout_msg.addWidget(QLabel(msg))
+        layout_main.addWidget(portrait)
+        layout_main.addLayout(layout_msg)
+        widget.setLayout(layout_main)
+        item = QListWidgetItem()
+        item.setSizeHint(QSize(200,70))
+        print('ddddd')
+        self.msg_list.addItem(item)
+        self.msg_list.setItemWidget(item, widget)
 
     def send_message(self):
         """
@@ -130,8 +163,8 @@ class ChatGUI(QWidget,Ui_Form):
         """
         empty_info = QLabel(self)
         empty_info.setText('不能发送空信息')
-        empty_info.setGeometry(QtCore.QRect(420, 160, 131, 20))
-        empty_info.setAlignment(QtCore.Qt.AlignCenter)
+        empty_info.setGeometry(QRect(420, 160, 131, 20))
+        empty_info.setAlignment(Qt.AlignCenter)
         empty_info.show()
         threading.Thread(target=self._close_label, args=(empty_info,)).start()
 
@@ -191,15 +224,15 @@ class ChatGUI(QWidget,Ui_Form):
         :return:
         """
         if obj is self.textEdit:  # 按下回车发送
-            if event.type() == QtCore.QEvent.KeyPress and (
-                    event.key() == QtCore.Qt.Key_Enter or event.key() == QtCore.Qt.Key_Return):
+            if event.type() == QEvent.KeyPress and (
+                    event.key() == Qt.Key_Enter or event.key() == Qt.Key_Return):
                 self.send_message()
                 return True  # 表示过滤此事件
 
         return False
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     id = 4
     user_name = '牛蛙丶丶'
     fav_comic = 'Attack on Titan'
