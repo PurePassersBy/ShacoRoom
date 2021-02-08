ebc142583fbee49c7c656b5b00cfeaeb2c206f4e
Craddy <1041396750@qq.com>
2021-02-08 19:57:50 +0800

    更改转换pixmap的方式（成功使用）

--- gui/ChatGui.py
+++ gui/ChatGui.py
@@ -212,9 +212,7 @@ class ChatGUI(QWidget, Ui_Form):
             shape = msg_pack['shape']
             image_np = np.frombuffer(msg_pack['image'], dtype='uint8').reshape(shape)
             image = Image.fromarray(image_np).convert('RGB')
-            print('...')
             pixmap = image.toqpixmap().scaled(400,200)
-            print('xxx')
             image_label = QLabel()
             image_label.setFixedSize(400,200)
             image_label.setPixmap(pixmap)
