b9d0604cfa8974e636269137057c9aa6c195b84b
GanbatteKnight <614446871@qq.com>
2021-02-14 22:53:14 +0800

    增加删除好友逻辑，每个用户唯一ini，分隔符为~

--- src/server/Manager.py
+++ src/server/Manager.py
@@ -116,23 +116,23 @@ class Manager(threading.Thread):
         :return:
         """
         config = configparser.ConfigParser()
-        file = config.read('add_friend.ini')
+        file = config.read(f'{str(user_id)}todo.ini')
         config_dict = config.defaults()
         print('Checking apply to do...')
-        key_name_to_delete = None
+        key_name_to_delete = []
         print(config_dict)
         for key_name in config_dict:
             print('Spot friend apply ')
-            split_key_name = key_name.split('-');
+            split_key_name = key_name.split('~')
             send_id = int(split_key_name[0])
             target_id = int(split_key_name[1])
             if target_id == user_id:
                 # 该用户id有待处理的好友请求信息
-                dict_temp = eval(config_dict[key_name])
-                cmd = list(dict_temp.keys())[0]
-                message = list(dict_temp.values())[0]
-                print(dict_temp, cmd, message, type(dict_temp.keys()))
-                if cmd == 'APPLY':
+                split_value = config_dict[key_name].split('~', 2)
+                type = split_value[0]
+                time = split_value[1]
+                message = split_value[2]
+                if type == 'APPLY':
                     # 需处理好友请求
                     print(f'Sending friend apply to {user_id}')
                     friend_apply_package = {
@@ -142,7 +142,7 @@ class Manager(threading.Thread):
                         'system_code': SYSTEM_CODE_FRIEND_APPLY}
                     send_package(self._user2conn[int(user_id)], friend_apply_package)
 
-                else:
+                if type == 'REPLY':
                     # 需处理好友请求结果
                     print(f'Sending result of friend apply to {user_id}')
                     friend_reply_package = {
@@ -152,13 +152,22 @@ class Manager(threading.Thread):
                         'system_code': SYSTEM_CODE_RESULT_FRIEND_APPLY}
                     send_package(self._user2conn[int(user_id)], friend_reply_package)
 
+                if type == 'DELETE':
+                    # 需解除好友关系结果
+                    print(f'Sending result of delete friend to {user_id}')
+                    package = {
+                        'send_id': send_id,
+                        'time': get_localtime(),
+                        'message': message,
+                        'system_code': SYSTEM_CODE_RESULT_DELETE_FRIEND}
+                    send_package(self._user2conn[int(user_id)], package)
                 # 删除服务器待处理好友请求中该用户的记录
-                key_name_to_delete = key_name
-                break
-        if key_name_to_delete is not None:
-            del config_dict[key_name_to_delete]
+                key_name_to_delete.append(key_name)
+
+        for i in key_name_to_delete:
+            del config_dict[i]
             # 用户上线后必须处理完好友请求，故不会出现send-target即是APPLY也是REPLY的情况
-            with open('add_friend.ini', 'w') as configfile:
+            with open(f'{user_id}todo.ini', 'a') as configfile:
                 config.write(configfile)
 
 
@@ -172,15 +181,16 @@ class Manager(threading.Thread):
         """
         print('Processing system code...')
         if pack['system_code'] == SYSTEM_CODE_FRIEND_APPLY:
-            config = configparser.ConfigParser()
-            file = config.read('add_friend.ini')
-            config_dict = config.defaults()
             send_id = pack['send_id']
             target_id = pack['target_id']
-            key_name = str(send_id) + '-' + str(target_id)
-            text = pack['message']
+            key_name = str(send_id) + '~' + str(target_id)
+            message = pack['message']
+            config = configparser.ConfigParser()
+            file = config.read(f'{str(target_id)}todo.ini')
+            config_dict = config.defaults()
             if key_name in config_dict:
                 # 重复发送好友请求
+                # 没成为好友就不会有离线私聊信息，故可以直接判断
                 print(f'send repeat friend apply information to sender:{send_id}')
                 repeat_apply_package = {
                     'time': get_localtime(),
@@ -196,26 +206,27 @@ class Manager(threading.Thread):
                     friend_apply_package = {
                         'send_id': send_id,
                         'time': get_localtime(),
-                        'message': text,
+                        'message': message,
                         'system_code': SYSTEM_CODE_FRIEND_APPLY}
                     send_package(self._user2conn[int(target_id)], friend_apply_package)
                 else:
                     # 当前好友请求目标用户不在线
-                    print(f'write {send_id}-{target_id}:[APPLY]{text} into add_friend.ini')
-                    dict_value = {'APPLY': text}
+                    print(f'write {send_id}~{target_id}:APPLY~{str(get_localtime())}~{str(message)}'
+                          f' into {str(target_id)}todo.ini')
+                    dict_value = f'APPLY~{str(get_localtime())}~{str(message)}'
                     config_dict[key_name] = dict_value
                     # 添加好友请求键值对：'发送-目标'--请求信息[类型：内容]
-                    with open('add_friend.ini', 'w') as configfile:
+                    with open(f'{str(target_id)}todo.ini', 'a') as configfile:
                         config.write(configfile)
 
         if pack['system_code'] == SYSTEM_CODE_RESULT_FRIEND_APPLY:
-            config = configparser.ConfigParser()
-            file = config.read('add_friend.ini')
-            config_dict = config.defaults()
             send_id = pack['send_id']
             target_id = pack['target_id']
             message = pack['message']
-            key_name = str(send_id) + '-' + str(target_id)
+            key_name = str(send_id) + '~' + str(target_id)
+            config = configparser.ConfigParser()
+            file = config.read(f'{str(target_id)}todo.ini')
+            config_dict = config.defaults()
             if target_id in self._user2conn:
                 # 当前好友请求发送用户在线
                 print(f'send result of friend apply information to target:{target_id}')
@@ -228,24 +239,25 @@ class Manager(threading.Thread):
                 send_package(self._user2conn[int(target_id)], result_package)
             else:
                 # 当前好友请求发送用户不在线
-                print(f'write {send_id}-{target_id}:[REPLY]{message} into add_friend.ini')
-                dict_value = {'REPLY': message}
+                print(f'write {send_id}~{target_id}:REPLY~{str(get_localtime())}~{str(message)} '
+                      f'into {str(target_id)}todo.ini')
+                dict_value = f'REPLY~{str(get_localtime())}~{str(message)}'
                 config_dict[key_name] = dict_value
                 # 添加好友请求回复键值对：'发送-目标'--好友请求结果[请求类型：内容]
-                with open('add_friend.ini', 'w') as configfile:
+                with open(f'{str(target_id)}todo.ini', 'a') as configfile:
                     config.write(configfile)
 
         if pack['system_code'] == SYSTEM_CODE_DELETE_FRIEND:
-            config = configparser.ConfigParser()
-            file = config.read('delete_friend.ini')
-            config_dict = config.defaults()
             send_id = pack['send_id']
             send_name = pack['send_name']
             target_id = pack['target_id']
             message = pack['message']
             key_name = str(send_id) + '-' + str(target_id)
+            config = configparser.ConfigParser()
+            file = config.read(f'{str(target_id)}todo.ini')
+            config_dict = config.defaults()
             if target_id in self._user2conn:
-                # 当前好友请求发送用户在线
+                # 当前解除好友结果目标用户在线
                 print(f'send result of delete friend information to target:{target_id}')
                 result_package = {
                     'send_id': send_id,
@@ -256,11 +268,12 @@ class Manager(threading.Thread):
                     'system_code': SYSTEM_CODE_RESULT_DELETE_FRIEND}
                 send_package(self._user2conn[int(target_id)], result_package)
             else:
-                # 当前好友请求发送用户不在线
-                print(f'write {send_id}-{target_id}:{message} into delete_friend.ini')
-                config_dict[key_name] = message
+                # 当前解除好友结果目标用户不在线
+                print(f'write {send_id}~{target_id}:{message} into {str(target_id)}todo.ini')
+                dict_value = f'DELETE~{str(get_localtime())}~{str(message)}'
+                config_dict[key_name] = dict_value
                 # 添加好友请求回复键值对：'发送-目标'--好友请求结果[请求类型：内容]
-                with open('delete_friend.ini', 'w') as configfile:
+                with open(f'{str(target_id)}todo.ini', 'a') as configfile:
                     config.write(configfile)
 
     def run(self):
